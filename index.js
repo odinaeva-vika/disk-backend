d054e93ea847725d0c56cceddacbce5aa97bcb9c
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-07-21 14:11:05 +0300

    fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const start = async () => {
             useUnifiedTopology:true
         })
 
-        app.listen(PORT, () => {
+        app.listen(PORT || 5000, () => {
             console.log('Server started on port ', PORT)
         })
     } catch (e) {
