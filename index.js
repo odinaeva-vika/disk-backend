5634dd717b2da234f7c4fed40ca77ab1331cb13d
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-07-21 21:09:02 +0300

    first commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const start = async () => {
             useUnifiedTopology:true
         })
 
-        app.listen(PORT || 5000, () => {
+        app.listen(PORT, () => {
             console.log('Server started on port ', PORT)
         })
     } catch (e) {
